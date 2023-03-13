3d50efafe09b3b677b0be10652de88806d965892
WdangTehSeeker3245 <faizalnf3245@gmail.com>
2023-03-13 18:51:36 +0700

    adding preloader

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,33 @@
-<script setup>
+<script>
 import Header from './components/Header.vue'
 import Project from './components/Project.vue'
 import Contact from './components/Contact.vue'
+import Preloader from './components/Preloader.vue';
+
+export default {
+    components: {
+      Preloader,Header,Project,Contact
+    },
+    data() {
+      return {
+        loading: true,
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.loading = false;
+      }, 10000);
+    },
+};
 </script>
 
 <template>
-  <Header/>
-  <Project/>
-  <Contact/>
+  <Preloader v-if="loading" />
+  <div v-if="!loading">
+    <Header/>
+    <Project/>
+    <Contact/>
+  </div>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/Preloader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="preloader">
+      <div class="spinner"></div>
+    </div>
+</template>
+  
+<style>
+    .preloader {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: lightblue;
+      z-index: 9999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .spinner {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      border: 5px solid #ccc;
+      border-top-color: #888;
+      animation: spinner 0.6s linear infinite;
+    }
+    @keyframes spinner {
+      to {
+        transform: rotate(360deg);
+      }
+    }
+</style>
+  
